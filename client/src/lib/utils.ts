e5c8b47d1e0189013b4761522a21280ac3fab260
Anirban Dutta <meanirbandutta22@gmail.com>
2025-02-27 16:47:08 +0530

    utils

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,19 @@
+// src/lib/utils.ts
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// Format date/time displays
+export function formatTime(timestamp: number): string {
+  return new Date(timestamp).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+export function formatDate(timestamp: number): string {
+  return new Date(timestamp).toLocaleDateString();
+}
