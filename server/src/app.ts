e0c8d9223998a9d180c6a776433c1b2125232a98
Anirban Dutta <meanirbandutta22@gmail.com>
2025-03-09 23:32:27 +0530

    cors added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 //internal imports
 import authRouter from "./routes/auth.route";
@@ -19,6 +20,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //parse cookies
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
 
 //set up routing
 app.use("/api/v1/auth", authRouter);
